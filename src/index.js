05a0b0944aaa32457ccf666070ba805d8a5bb13d
Adrian Lee <adrianlee0118@gmail.com>
2020-07-28 19:02:25 -0700

    added features

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import ReactDOM from "react-dom";
+import classNames from "classnames";
 import "./index.css";
 
 const Square = ({ value, onClick }) => (
@@ -29,30 +30,116 @@ const calculateWinner = (squares) => {
 };
 
 const Board = ({ squares, onClick }) => {
-  const renderSquare = (i) => (
-    <Square value={squares[i]} onClick={() => onClick(i)} />
-  );
+  const renderSquare = (i) => {
+    console.log("squares in Board: " + squares);
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
+  };
   return (
     <div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
+      {[
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+      ].map((row) => {
+        return (
+          <div className="board-row">{row.map((i) => renderSquare(i))}</div>
+        );
+      })}
+    </div>
+  );
+};
+
+const gameReducer = (state, action) => {
+  switch (action.type) {
+    case "HANDLE_CLICK":
+      return {
+        ...state,
+        history: action.history,
+        stepNumber: action.stepNumber,
+        xIsNext: action.xIsNext,
+      };
+    case "JUMP_TO":
+      return {
+        ...state,
+        stepNumber: action.step,
+        xIsNext: action.step % 2 === 0,
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
+};
+
+const Game = () => {
+  const [state, dispatch] = useReducer(gameReducer, {
+    history: [{ squares: Array(9).fill(null), moveI: null, moveJ: null }],
+    stepNumber: 0,
+    xIsNext: true,
+  });
+
+  const handleClick = (i) => {
+    const hist = state.history.slice(0, state.stepNumber + 1);
+    const curr = hist[hist.length - 1];
+    const squares = curr.squares.slice();
+    if (calculateWinner(squares) || squares[i]) return;
+    squares[i] = state.xIsNext ? "X" : "O";
+    const moveX = (i % 3) + 1;
+    const moveY = i < 3 ? 1 : i >= 6 ? 3 : 2;
+    dispatch({
+      type: "HANDLE_CLICK",
+      history: hist.concat([{ squares: squares, moveI: moveX, moveJ: moveY }]),
+      stepNumber: hist.length,
+      xIsNext: !state.xIsNext,
+    });
+  };
+
+  const jumpTo = (step) => {
+    dispatch({ type: "JUMP_TO", step: step });
+  };
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board
+          squares={state.history[state.stepNumber].squares}
+          onClick={(i) => handleClick(i)}
+        />
       </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
+      <div className="game-info">
+        <div>
+          {calculateWinner(state.history[state.stepNumber].squares)
+            ? "Winner: " +
+              calculateWinner(state.history[state.stepNumber].squares)
+            : "Next player: " + (state.xIsNext ? "X" : "O")}
+        </div>
+        <ol>
+          {
+            //map: First arg is value of element, second arg is index, third is array itself according to MDN
+            state.history.map((step, move) => (
+              <li key={move}>
+                <button
+                  onClick={() => jumpTo(move)}
+                  className={classNames({
+                    "button-active": move === state.stepNumber,
+                  })}
+                >
+                  {move
+                    ? `Go to move#${move} by ${
+                        move % 2 === 0 ? "O" : "X"
+                      } at (${step.moveI},${step.moveJ})`
+                    : "Go to game start"}
+                </button>
+              </li>
+            ))
+          }
+        </ol>
       </div>
     </div>
   );
 };
 
+/*
 const Game = () => {
   const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
   const [stepNumber, setStepNumber] = useState(0);
@@ -62,6 +149,7 @@ const Game = () => {
     const hist = history.slice(0, stepNumber + 1);
     const curr = hist[hist.length - 1];
     const squares = curr.squares.slice();
+    console.log("squares created: " + squares);
     if (calculateWinner(squares) || squares[i]) return;
     squares[i] = xIsNext ? "X" : "O";
     setHistory(hist.concat([{ squares: squares }]));
@@ -101,6 +189,7 @@ const Game = () => {
     </div>
   );
 };
+*/
 
 // ========================================
 
